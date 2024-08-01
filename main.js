3de9bfb63ecca320735b18a753a6fb2a9a16cefe
retrolous <lukaspetkunas07@gmail.com>
2024-08-01 15:12:52 +0300

    Add dynamic elements to the website
    The populateWrapper function was rewritten to take a number as a
    parameter and generate a grid with side length of that number
    
    Button and input were added to be able to submit new grid lengths and
    input values for them, respectively
    
    A CSS animation was added to the input box so that an invalid value -
    less than 1 and more than 100 - can be easily recognised by the user and
    consequently fixed

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
-const wrapperDiv = document.querySelector("#wrapper");
+
 
 function populateWrapper(noOfItemsPerRow){
+    const wrapperDiv = document.querySelector("#wrapper");
     for(i = 0; i < (noOfItemsPerRow ** 2); i++){
         let newDiv = document.createElement("div");
         wrapperDiv.appendChild(newDiv);
+        newDiv.style.height = `${100 / noOfItemsPerRow}%`
+        newDiv.addEventListener("mouseover", () => {
+        newDiv.classList.add("hovered")
+        })
     }
 }
+function initialiseButton(){
+    const button = document.querySelector("button");
+    const numInput = document.querySelector("input");
+    
 
-populateWrapper(16)
+    numInput.addEventListener("animationend", (e) => {
+        numInput.classList.remove("apply-shake");
+    });
+    
+}
+initialiseButton()
+populateWrapper(10)
